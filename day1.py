3036e3e2cab0843ea0a9eb1bbc20fcc5038bb2f3
Fredrik Bengtsson <fredrik.bengtsson94@gmail.com>
2018-12-01 09:03:06 +0100

    Day 1 solved

---
+++ day1.py
@@ -0,0 +1,30 @@
+
+def puzzel1():
+    freq = 0
+    with open("day1_data.txt") as f:
+        for line in f:
+            freq += int(line)
+    print(freq)
+
+def puzzel2():
+    with open("day1_data.txt") as f:
+        changes = [int(line) for line in f]
+    
+    same_freq_found = False
+    freqs = [changes[0]]
+    freq = changes[0]
+    idx = 1
+    while not same_freq_found:
+        if idx == len(changes):
+            idx = 0
+        freq += changes[idx]
+        if freq in freqs:
+            print(freq)
+            same_freq_found = True
+            break
+        else:
+            freqs.append(freq)
+            idx += 1
+
+if __name__ == "__main__":
+    puzzel2()
