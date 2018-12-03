53767fa691f0dc5fc3c3a1229eccc9a7c388a524
Fredrik Bengtsson <fredrik.bengtsson94@gmail.com>
2018-12-03 10:39:31 +0100

    day2: add oneliner solution for puzzel 1

--- day2/day2.py
+++ day2/day2.py
@@ -21,20 +21,26 @@ def puzzel1():
 
     return twos * threes
 
-
 def puzzel2():
     ids = [line.strip() for line in open("day2_data.txt")]
     id_len = len(ids[0])
     for char_idx in range(id_len):
-        ids_one_removed = list(map(lambda c: (c[:char_idx] + c[char_idx+1:id_len], c[char_idx]), ids))
-        ids_one_removed = sorted(ids_one_removed, key=lambda pair: pair[0])
-        last_id_pair = ("","")
-        for id_pair in ids_one_removed:
-            if last_id_pair[0] == id_pair[0]:
-                #print(f'{last_id_pair}, {id_pair}')
-                return id_pair[0]
+        ids_one_removed = list(map(lambda c: c[:char_idx] + c[char_idx+1:id_len], ids))
+        ids_one_removed = sorted(ids_one_removed)
+        last_id = ""
+        for cur_id in ids_one_removed:
+            if cur_id == last_id:
+                return cur_id
             else:
-                last_id_pair = id_pair
+                last_id = cur_id
+
+from itertools import groupby
+from functools import reduce 
+from operator import mul
+def puzzel1_short():
+    return mul(*(reduce(lambda p1, p2: (p1[0]+ p2[0], p1[1] + p2[1]) ,map(lambda counts: (2 in counts, 3 in counts) ,map(lambda cur_id:  [len(list(g)) for k,g in groupby(sorted(cur_id))],[line.strip() for line in open("day2_data.txt")])))))
+
 
 print(f'puzzel 1: {puzzel1()}')
-print(f'puzzel 2:{puzzel2()}')
+print(f'puzzel 1 - one liner: {puzzel1_short()}')
+print(f'puzzel 2: {puzzel2()}')
