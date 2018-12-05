d5cafd122cf94cb885fd40b2d5667054cbf1ccec
Fredrik Bengtsson <fredrik.bengtsson94@gmail.com>
2018-12-05 16:20:53 +0100

    day5: make a much faster solution

--- day5/day5.py
+++ day5/day5.py
@@ -1,43 +1,32 @@
 import string
 
-def puzzel1(polymer):
-    reacted = True
-    while reacted:
-        #print(len(polymer))
-        reacted = False
-        last_elem = ""
-        for idx, elem in enumerate(polymer):
-            #print(f'{last_elem}:{elem}')
-            if elem.islower() and elem.upper() == last_elem:
-                reacted = True
-                rm_idx = idx
-                break
-            elif elem.isupper() and elem.lower() == last_elem:
-                reacted = True
-                rm_idx = idx
-                break
-            last_elem = elem
-        if reacted:
-            #print(rm_idx)
-            #print(f'remmoving: {polymer[rm_idx-1]+polymer[rm_idx]} ')
-            polymer = polymer[:rm_idx-1] + polymer[rm_idx+1:]
-    #print("DONE!, printing polymer")
-    #print(polymer)
+def puzzel1_fast(polymer):
+    reactions = reactions = [x[0]+x[1] for x in zip(string.ascii_lowercase + string.ascii_uppercase, string.ascii_uppercase+string.ascii_lowercase)]
+    last_len = float("inf")
+    while len(polymer) != last_len:
+        if(polymer == ""):
+            return 0
+        last_len = len(polymer)
+        for reaction in reactions:
+            polymer = polymer.replace(reaction,"")
     print(f'length of polymer: {len(polymer)}')
     return len(polymer)
 
 def puzzel2(polymer):
+    min_len = float("inf")
     for character in string.ascii_lowercase:
         print(f'Filtering: {character}')
-        min_len = float("inf")
-        new_poly = "".join(list(filter(lambda e: e.lower() != character ,polymer)))
-        pol_length = puzzel1(new_poly)
+        new_poly = polymer.replace(character, "")
+        new_poly = new_poly.replace(character.upper(), "")
+        pol_length = puzzel1_fast(new_poly)
         if pol_length <= min_len:
             min_len = pol_length
     return min_len
 
 
-
 polymer = "".join([line for line in open("day5_data.txt")]).strip()
-print(puzzel1(polymer))
-print(puzzel2(polymer))
+print(f'puzzel1: {puzzel1_fast(polymer)}')
+print("----------------------------------------")
+print(f'puzzel2: {puzzel2(polymer)}')
+
+
